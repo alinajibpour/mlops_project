9d42b79d554bdf341966a914ce3ac86500fbafa2
alinajibpour <ali.najibpour68@gmail.com>
2024-06-18 14:44:09 +0200

    some changes are made e.g. github action and unit testing

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# tests/__init__.py
+
+import os
+
+_TEST_ROOT = os.path.dirname("C:\Users\ra59xaf\Desktop\my_project\mlops\tests")  # root of test folder
+_PROJECT_ROOT = os.path.dirname("C:\Users\ra59xaf\Desktop\my_project\mlops")  # root of project
+_PATH_DATA = os.path.join("C:\Users\ra59xaf\Desktop\my_project\mlops\data", "data")  # root of data
